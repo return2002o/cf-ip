f5164ac02befdb030219866531f75716ae9d2982
return2002o <106457849+return2002o@users.noreply.github.com>
2025-01-06 21:26:28 +0800

    end

--- .github/workflows/ip_get.py
+++ .github/workflows/ip_get.py
@@ -9,7 +9,7 @@ def main():
     url = 'https://ip.164746.xyz/ipTop.html'
     resp = requests.get(url)
     if resp.status_code == 200:
-        ip_domin = []
+        ip_domin = [] 
         for ip in resp.text.split(','):
             # 去除可能的空格和换行符
             ip = ip.strip()
