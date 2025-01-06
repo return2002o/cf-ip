6bcde6a8bcc927f76843d9e1a357649027eacc58
return2002o <106457849+return2002o@users.noreply.github.com>
2025-01-06 21:06:27 +0800

    end

--- .github/workflows/ip_get.py
+++ .github/workflows/ip_get.py
@@ -16,7 +16,7 @@ def main():
             if ip:
                 ip_domin.append(ip + '.example.com')
         # 写入文件 README.md
-        readme_path = ".github/workflows/README.md"
+        readme_path = "README.md"
         with open(readme_path, 'w') as f:
             f.write("# IP Addresses with example.com\n")
             for ip in ip_domin:
